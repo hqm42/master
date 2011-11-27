049c4e29485320890f493b5d72ffe73fe360c3b0
Robert Steuck <robert.steuck@gmail.com>
2011-11-27 12:05:26 +0100

    cleaned up example code

--- Hallo.hs
+++ Hallo.hs
@@ -8,18 +8,17 @@ j2s jsv = s
     Ok s = fromJSON jsv
 
 main = runHWTApp $ do
-  v1 <- transientValue ""
-  sv1 <- serverValue ""
-  sv2 <- serverValue (0 :: Int)
-  m1 <- readModel v1
-  m2 <- readWriteModel v1
-  m3 <- readModel sv1
-  m4 <- readModel sv2
-  t1 <- textField m2 Nothing Nothing
-  b1 <- button m4 (\newMessage -> do
-                          chatLog <- getValue sv1
-                          setValue sv1 $ chatLog ++ "\n" ++ newMessage
-                          msgCount <- getValue sv2
-                          setValue sv2 $ msgCount + 1) v1
-  l2 <- label m3 Nothing
-  panel Nothing [l2,t1,b1]
+  message <- transientValue ""
+  chatLog <- serverValue ""
+  msgCounter <- serverValue (0 :: Int)
+  messageModel <- readWriteModel message
+  chatLogModel <- readModel chatLog
+  msgCounterModel <- readModel msgCounter
+  messageInput <- textField messageModel Nothing Nothing
+  sendButton <- button msgCounterModel (\newMessage -> do
+                          oldMessages <- getValue chatLog
+                          setValue chatLog $ oldMessages ++ " " ++ newMessage
+                          cnt <- getValue msgCounter
+                          setValue msgCounter $ cnt + 1) message
+  chatLogOutput <- label chatLogModel Nothing
+  panel Nothing [messageInput,sendButton,chatLogOutput]
